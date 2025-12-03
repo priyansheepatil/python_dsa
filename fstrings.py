475997d98ba7065afcfde4c95dd790897e2b87a8
priyansheepatil <priyanshee.patil@gmail.com>
2025-12-03 23:40:41 +0530

    Add files via upload

---
+++ fstrings.py
@@ -0,0 +1,14 @@
+name = "Pree"
+age = 21
+text = (f"My name is {name} and my age is {age}")
+print(text)
+
+name = input("Enter your name:")
+city = input("Enter your city name:")
+
+print(f"My name is {name} and I live in {city}")
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print(f"The addition of x and y is {x+y}")
+
